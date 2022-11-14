c8c8f11676627db885e9b99a3f2b4557dae1e9f3
ShivaTejaV <116464067+ShivaTejaV@users.noreply.github.com>
2022-11-14 17:01:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get("/players/:playerId/", async (request, response) => {
 app.put("/players/:playerId/", async (request, response) => {
   const id = parseInt(request.params.playerId);
   const { playerName } = request.body;
-  console.log(id, playerName);
+  //console.log(id, playerName);
   const q3 = `
     UPDATE 
         player_details
@@ -80,26 +80,58 @@ app.put("/players/:playerId/", async (request, response) => {
 
 //API 4
 
-app.get("/matches/:matchId/", async (Request, response) => {
+app.get("/matches/:matchId/", async (request, response) => {
   const id = parseInt(request.params.matchId);
   const q4 = `
     SELECT *
     FROM match_details
-    WHERE match_id = ${id};`;
+    WHERE match_id =${id}`;
   const dbResponse = await db.get(q4);
-  const temp = {
+  let temp = {
     matchId: dbResponse.match_id,
     match: dbResponse.match,
     year: dbResponse.year,
   };
-  console.log(temp);
   response.send(temp);
 });
 
 //API 5
 
+app.get("/players/:playerId/matches", async (request, response) => {
+  const id = parseInt(request.params.playerId);
+  const q5 = `
+    SELECT match_id as matchId,match,year
+    FROM (player_details NATURAL JOIN player_match_score) NATURAL JOIN match_details
+    WHERE player_id = ${id};`;
+  const dbResponse = await db.all(q5);
+  //console.log(dbResponse);
+  response.send(dbResponse);
+});
+
 //API 6
 
+app.get("/matches/:matchId/players", async (request, response) => {
+  const id = parseInt(request.params.matchId);
+  const q6 = `
+    SELECT player_id as playerId,player_name as playerName
+    FROM (match_details NATURAL JOIN player_match_score) NATURAL JOIN player_details;
+    `;
+  const dbResponse = await db.all(q6);
+  console.log(dbResponse);
+  response.send(dbResponse);
+});
+
 //API 7
+
+app.get("/players/:playerId/playerScores", async (request, response) => {
+  const id = parseInt(request.params.playerId);
+  const q7 = `
+    SELECT su(score)
+    FROM (player_details NATURAL JOIN player_match_score) NATURAL JOIN match_details
+    WHERE player_id = ${id};`;
+  const dbResponse = await db.all(q7);
+  console.log(dbResponse);
+  response.send(dbResponse);
+});
 
 module.exports = app;
